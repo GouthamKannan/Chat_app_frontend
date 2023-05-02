3c0fbf1ea86cf9c7ca9a84a91cb190745f30a260
Goutham Kannan <54542723+GouthamKannan@users.noreply.github.com>
2023-05-02 09:46:01 +0530

    Update backend url

--- src/config.js
+++ src/config.js
@@ -1,6 +1,8 @@
 var configs = {
-    api_url : "https://group-chat-application-test.herokuapp.com",
-    socket_url : "https://chat-app-socket-server-test.herokuapp.com"
+    // api_url : "https://group-chat-application-test.herokuapp.com",
+    // socket_url : "https://chat-app-socket-server-test.herokuapp.com"
+    api_url : "https://group-chat-application-test.onrender.com",
+    socket_url : "https://chat-app-socket-server-test.onrender.com"
 }
 
 export default configs
